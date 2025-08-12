a6fe8e8015cf1b0a229dc0549b1b297d5eb43384
palpatel09 <palgpatel07@gmail.com>
2025-08-12 17:08:43 +0530

    Add files via upload

---
+++ if-else.c
@@ -0,0 +1,15 @@
+// Online C compiler to run C program online
+#include<stdio.h>
+int main()
+{
+printf("Enter number\n");
+int number;
+scanf("%d",&number);
+if(number%2==0){
+printf("Number is Even");
+}
+else{
+printf("Number is Odd");
+}
+return 0;
+}
