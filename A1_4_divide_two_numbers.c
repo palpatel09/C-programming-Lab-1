713e047da274a4d0fbea5485fea72054915f7e16
palpatel09 <palgpatel07@gmail.com>
2025-08-13 21:57:41 +0530

    Add files via upload

---
+++ A1_4_divide_two_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+//Question.4 divide two numbers
+int main() {
+    int n1,n2;
+    printf("enter first number:");
+    scanf("%d",&n1);
+    printf("enter second number:");
+    scanf("%d",&n2);
+    if (n2==0) {
+        printf("division by 0 is not possible");
+    }
+    else {
+        int division = n1 / n2;
+        printf("division of inputed numbers is: %d", division);
+    }
+    return 0;
+}
